110bb43664a14e85734538a9450606ff2ecde1a5
stuartmcf <stuart.mcfarland@gosource.com.au>
2023-06-16 13:11:02 +1000

    v6

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> This is v5.
+          Edit <code>src/App.js</code> This is v6.
         </p>
         <a
           className="App-link"
