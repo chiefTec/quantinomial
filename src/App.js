6deeccd4020d7d90fa0064f7cc9915d2f3c11bc3
stuartmcf <stuart.mcfarland@gosource.com.au>
2023-06-16 13:06:03 +1000

    v5 commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> This is v3.
+          Edit <code>src/App.js</code> This is v5.
         </p>
         <a
           className="App-link"
